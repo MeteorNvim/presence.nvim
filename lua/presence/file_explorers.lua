e859b9615c944096c2f5d43c1c59d020a1ad2e32
Fadel <59105868+SrWither@users.noreply.github.com>
2022-08-15 01:41:06 -0300

    Update file_explorers.lua

--- lua/presence/file_explorers.lua
+++ lua/presence/file_explorers.lua
@@ -1,6 +1,7 @@
 -- Different neovim file explorer names keyed by filetype or buffer name
 return {
-    ["NvimTree"] = "NvimTree",
+    ["NvimTree"] = "text",
+    ["Term"] = "terminal",
     ["NERD_tree_"] = "NERDTree",
     ["[defx] default-"] = "Defx",
     ["netrw"] = "Netrw",
